f0209a9f5fb0781328130ba19824f01480473cfd
Sh3dOz <jayden6567@gmail.com>
2023-04-23 12:31:53 +0800

    Updated enemy script

--- GDDS 2/Assets/Jayden's Scripts/ShootingEnemy.cs
+++ GDDS 2/Assets/Jayden's Scripts/ShootingEnemy.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class ShootingEnemy : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public Vector3 offset = new Vector3(10f, 0f, 0f);
+    public float size = 10f;
+    public int bulletCount;
+    public int maxBullet;
+    public Transform shootPos;
     public float fireRate;
-    public float nextFire;
+    float nextFire;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +21,24 @@ public class ShootingEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Time.deltaTime < nextFire)
-        {
-
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position + offset, new Vector2(size,size), 0, -transform.right);
+        if (bulletCount > 0) 
+        { if (Time.time < nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                Instantiate(bulletPrefab, shootPos.position, Quaternion.identity);
+                bulletCount--;
+            }
         }
+        else
+        {
+            waitReload();
+        }
+    }
+
+    IEnumerator waitReload()
+    {
+        yield return new WaitForSeconds(3f);
+        bulletCount = maxBullet;
     }
 }
