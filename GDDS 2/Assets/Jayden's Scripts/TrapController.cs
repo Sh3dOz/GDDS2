abd2a490f62fbafb81ee8be73d74d0e9f67ff9a7
Sh3dOz <jayden6567@gmail.com>
2023-05-05 12:18:24 +0800

    Created random trap spawn

--- GDDS 2/Assets/Jayden's Scripts/TrapController.cs
+++ GDDS 2/Assets/Jayden's Scripts/TrapController.cs
@@ -6,17 +6,21 @@ public class TrapController : MonoBehaviour
 {
     public List<Traps> possibleTraps;
     public SpriteRenderer sr;
+    int damage;
+    string trapName;
     Traps currentTrap;
+    float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        GenerateTrap();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        GenerateTrapBehaviour(trapName);
     }
 
     public void GenerateTrap()
@@ -24,5 +28,28 @@ public class TrapController : MonoBehaviour
         int rand = Random.Range(0, possibleTraps.Count);
         currentTrap = possibleTraps[rand];
         sr.sprite = currentTrap.sprite;
+        damage = currentTrap.damage;
+        trapName = currentTrap.trapName;
+    }
+
+    public void GenerateTrapBehaviour(string trapName)
+    {
+        switch (trapName)
+        {
+            case ("Buzzsaw"):
+            case ("Dash"):
+                moveSpeed = currentTrap.moveSpeed;
+                transform.position = new Vector3(transform.position.x + moveSpeed, transform.position.y, transform.position.z);
+                break;
+            default: break;
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>())
+        {
+            collision.GetComponent<PlayerController>().TakeDamage(damage);
+        }
     }
 }
--- GDDS 2/Assets/Jayden's Scripts/Traps.cs
+++ GDDS 2/Assets/Jayden's Scripts/Traps.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[CreateAssetMenu(menuName = "Traps")]
 public class Traps : ScriptableObject
 {
+    public string trapName;
     public Sprite sprite;
+    public int damage;
+    public float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +19,5 @@ public class Traps : ScriptableObject
     {
         
     }
+    
 }
