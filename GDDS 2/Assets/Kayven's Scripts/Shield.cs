ad082ec24c58c202bf7650ef84cc78796e257fd8
Kayven-Chong <kayvenchong@gmail.com>
2023-05-07 20:17:57 +0800

    Shield
    Shield destroy in 3 hits

---
+++ GDDS 2/Assets/Kayven's Scripts/Shield.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+
+    [Header("Shield heath")]
+    public float shieldHealth = 3;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        shieldHealth = 3;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(shieldHealth <= 0){
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.GetComponent<Bullet>()) {
+            shieldHealth -= 1;
+        }
+    }
+}
