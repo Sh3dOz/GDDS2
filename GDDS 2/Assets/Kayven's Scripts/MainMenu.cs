b18424edc62fe9df826dcb7058eeb96ddb34aa16
Kayven-Chong <kayvenchong@gmail.com>
2023-05-23 15:05:28 +0800

    UI

--- GDDS 2/Assets/Kayven's Scripts/MainMenu.cs
+++ GDDS 2/Assets/Kayven's Scripts/MainMenu.cs
@@ -1,30 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
     // Start is called before the first frame update
 
     public string loadLevel;
+    public AudioSource audioSource;
+    public AudioClip pressingSound;
+
     void Start() {
+
     }
     // Load game.
     public void NewGame() {
+        StartCoroutine("StartGame");
+    }
+
+    public IEnumerator StartGame() {
+        audioSource.PlayOneShot(pressingSound);
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(loadLevel);
     }
     public void Credits() {
+        StartCoroutine("Credit");
+    }
+    public IEnumerator Credit() {
+        audioSource.PlayOneShot(pressingSound);
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene("Credits");
     }
+
     public void Settings() {
+        StartCoroutine("Setting");
+    }
+
+    public IEnumerator Setting() {
+        audioSource.PlayOneShot(pressingSound);
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene("Settings");
     }
 
     public void Instructions() {
+        StartCoroutine("Instruction");
+    }
+
+    public IEnumerator Instruction() {
+        audioSource.PlayOneShot(pressingSound);
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene("Instructions");
     }
     // Quit game.
     public void QuitGame() {
         Application.Quit();
+
     }
 }
