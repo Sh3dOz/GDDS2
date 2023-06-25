8556ba7b3de6baa852c3d240c1e620f1e901d3c2
Kayven-Chong <kayvenchong@gmail.com>
2023-06-25 22:54:24 +0800

    Added zap to laser obstacles

--- GDDS 2/Assets/Jayden's Scripts/CollisionTrapController.cs
+++ GDDS 2/Assets/Jayden's Scripts/CollisionTrapController.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class CollisionTrapController : MonoBehaviour
 {
+    public AudioSource audioS;
+    public AudioClip shockSound;
+    public GameObject shockEffect;
+    public GameObject player;
+
+
     public int damage;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,9 +25,12 @@ public class CollisionTrapController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<PlayerController>())
+        if (collision.GetComponentInParent<PlayerController>())
         {
-            collision.GetComponent<PlayerController>().TakeDamage(damage);
+            collision.GetComponent<PlayerController>().TakeDamage(damage);           
+            Instantiate(shockEffect, player.transform.position, Quaternion.identity);
+            audioS.PlayOneShot(shockSound);
+            Debug.Log("ZAPped");
         }
     }
 }
