b244058d5345b8375f01c3c47de41493302f8041
Sh3dOz <jayden6567@gmail.com>
2023-06-27 16:30:20 +0800

    Drill enemy done laser enemy half done

--- GDDS 2/Assets/Jayden's Scripts/DashTrap.cs
+++ GDDS 2/Assets/Jayden's Scripts/DashTrap.cs
@@ -5,43 +5,51 @@ using UnityEngine;
 
 public class DashTrap : MonoBehaviour
 {
-    float moveSpeed;
+    float dashSpeed = -10f;
     bool isDashing = false;
     public Transform dashPos;
     bool dashWait;
     public GameObject target;
     public int damage;
+    Animator myAnim;
+    bool isActivated;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
 
     void Movement()
     {
-        StartCoroutine(DashWait());
-        if (isDashing == false)
+        if (isActivated)
         {
-            transform.position = new Vector3(dashPos.position.x, target.transform.position.y, transform.position.z);
-        }
-        else if (isDashing == true)
-        {
-            transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            if (isDashing == false)
+            {
+                transform.position = new Vector3(dashPos.position.x, target.transform.position.y, transform.position.z);
+            }
+            else if (isDashing == true)
+            {
+                transform.position = new Vector3(transform.position.x + (dashSpeed * Time.deltaTime), transform.position.y, transform.position.z);
+            }
+            Debug.Log("yes?");
         }
     }
 
     IEnumerator DashWait()
     {
-        if (dashWait == true) yield break;
+        isActivated = true;
         dashWait = true;
         yield return new WaitForSeconds(3f);
+        dashWait = false;
+        GetComponent<PolygonCollider2D>().isTrigger = true;
         isDashing = true;
+        Debug.Log("done?");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -49,6 +57,12 @@ public class DashTrap : MonoBehaviour
         if (collision.GetComponent<PlayerController>())
         {
             collision.GetComponent<PlayerController>().TakeDamage(damage);
+            Destroy(this.gameObject);
+        }
+        else if (collision.GetComponent<CameraCollider>())
+        {
+            myAnim.enabled = true;
+            StartCoroutine("DashWait");
         }
     }
 }
--- GDDS 2/Assets/Jayden's Scripts/LaserController.cs
+++ GDDS 2/Assets/Jayden's Scripts/LaserController.cs
@@ -7,30 +7,54 @@ public class LaserController : MonoBehaviour
     public Transform spawnPosition;
     public Transform targetPosition;
     public bool isActivated;
+    public GameObject laserBeam;
+    public bool isEnemy;
+
+    public PolygonCollider2D smallLaser;
+    public PolygonCollider2D bigLaser;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isActivated)
+        if (isEnemy)
         {
-            transform.position = spawnPosition.position;
-            transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetPosition.position.x, Time.deltaTime), transform.position.y, transform.position.z);
-
-            Vector2 pos = new Vector2(transform.position.x, transform.position.z),
-                targ = new Vector2(targetPosition.position.x, targetPosition.position.z),
-                dist = pos - targ;
-
-            if (dist.sqrMagnitude < 0.0001f)
+            if (isActivated)
             {
-                transform.parent = FindObjectOfType<Camera>().gameObject.transform;
-                 //Charge Laser while moving backwards
+                transform.position = new Vector2(Mathf.Lerp(transform.position.x, targetPosition.position.x, 1f * Time.deltaTime), Mathf.Lerp(transform.position.y, targetPosition.position.y, 1f * Time.deltaTime));
 
+                Vector2 pos = new Vector2(transform.position.x, transform.position.z),
+                    targ = new Vector2(targetPosition.position.x, targetPosition.position.z),
+                    dist = pos - targ;
+
+                if (dist.sqrMagnitude < .1f)
+                {
+                    transform.parent = FindObjectOfType<Camera>().gameObject.transform;
+                    //Charge Laser while moving backwards
+                    laserBeam.SetActive(true);
+                }
             }
         }
+    }
+
+    public void SmolLaser()
+    {
+        smallLaser.enabled = true;
+    }
+
+    public void ChunkyLaser()
+    {
+        smallLaser.enabled = false;
+        bigLaser.enabled = true;
+    }
+
+    public void DiaableLaser()
+    {
+        smallLaser.enabled = false;
+        bigLaser.enabled = false;
     }
 }
