487598c7c3e6b72edc76603a9dd2f7b9dfbb07d9
Kayven-Chong <kayvenchong@gmail.com>
2023-07-01 18:26:48 +0800

    transition half done

--- GDDS 2/Assets/Kayven's Scripts/CameraTargetChanger.cs
+++ GDDS 2/Assets/Kayven's Scripts/CameraTargetChanger.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Playables;
+
+
 public class CameraTargetChanger : MonoBehaviour {
+
+    public Transform transition;
     public Transform newTarget;
     public CinemachineVirtualCamera virtualCamera;
     public TransitionToSpaceship spaceship;
 
+    public PlayableDirector cameraDirector;
+
+    public float smoothing = 2f;
+
     void Start() {
-        spaceship = FindObjectOfType<TransitionToSpaceship>();    
+        spaceship = FindObjectOfType<TransitionToSpaceship>();
     }
     void Update() {
         if (spaceship.transitioned == true) {
@@ -17,5 +26,15 @@ public class CameraTargetChanger : MonoBehaviour {
     }
     void ChangeCameraTarget() {
         virtualCamera.Follow = newTarget;
+    }
+
+    public void CameraTransition() {
+        virtualCamera.Follow = null;
+        StartCoroutine("CameraToSpace");
+    }
+
+    public IEnumerator CameraToSpace() {
+        yield return new WaitForSeconds(2f);
+        cameraDirector.Play();
     }
 }
