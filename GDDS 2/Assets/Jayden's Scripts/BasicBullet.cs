7ecd8708f186659988e2aaba3b9ba34de7dac7ed
Kayven-Chong <kayvenchong@gmail.com>
2023-07-10 19:02:09 +0800

    gonna have to add different effects particles for diff objects later

--- GDDS 2/Assets/Jayden's Scripts/BasicBullet.cs
+++ GDDS 2/Assets/Jayden's Scripts/BasicBullet.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicBullet : Bullet
+
 {
+
+    public AudioSource audioS;
+    public AudioClip shotSound;
+    public GameObject shotEffect;
+    public PlayerController[] players;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        players = FindObjectsOfType<PlayerController>();
+        audioS = FindObjectOfType<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,6 +32,20 @@ public class BasicBullet : Bullet
         if (timeSpawned > 2f)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.GetComponentInParent<PlayerController>()) {
+            collision.GetComponent<PlayerController>().TakeDamage(damage);
+            foreach (PlayerController player in players) {
+                Instantiate(shotEffect, player.transform.position, Quaternion.identity);
+            }
+            audioS.PlayOneShot(shotSound);
+            Debug.Log("ZAPped");
+        }
+        if (collision.tag == "EMP") {
+            Destroy(this.gameObject);
         }
     }
 }
