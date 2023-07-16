ff1e411753d4d7734e8c18295bb047be6eb95e03
Sh3dOz <jayden6567@gmail.com>
2023-07-16 22:03:16 +0800

    UI should be fixed + revamped upgrades to work universally

---
+++ GDDS 2/Assets/Jayden's Scripts/PassivePanel.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PassivePanel : MonoBehaviour
+{
+    [Header("Ability")]
+    public bool isPassive;
+    public bool isSkill;
+    public bool isPowerup;
+    public bool isHealth;
+    public int abilityIndex;
+    string abilityName;
+    public TMP_Text abilityText;
+
+    [Header("ItemsToChange")]
+    public Image coinImage;
+    public Text coinCost;
+    public Image firstBar, secondBar, thirdBar;
+    public Sprite firstEmpty, secondEmpty, thirdEmpty, firstFilled, secondFilled, thirdFilled;
+    public List<float> upgradeCost;
+    public List<string> upgradeText;
+    public GameObject upgradeButton;
+    public GameObject maxedPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isPassive)
+        {
+            abilityName = "Passive";
+        }
+        if (isSkill)
+        {
+            abilityName = "Skill";
+        }
+        if (isPowerup)
+        {
+            abilityName = "Powerup";
+        }
+        if (isHealth)
+        {
+            abilityName = "Health";
+        }
+        UpdatePoints();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePoints();
+    }
+
+    public void UpdatePoints()
+    {
+        abilityIndex = PlayerPrefs.GetInt(UpgradesAgain.selectedCharacter + abilityName);
+        switch (abilityIndex)
+        {
+            case 0:
+                coinCost.text = upgradeCost[abilityIndex].ToString();
+                abilityText.text = upgradeText[abilityIndex];
+                firstBar.sprite = firstEmpty;
+                secondBar.sprite = secondEmpty;
+                thirdBar.sprite = thirdEmpty;
+                break;
+
+            case 1:
+                coinCost.text = upgradeCost[abilityIndex].ToString();
+                abilityText.text = upgradeText[abilityIndex];
+                firstBar.sprite = firstFilled;
+                secondBar.sprite = secondEmpty;
+                thirdBar.sprite = thirdEmpty;
+                break;
+            case 2:
+                coinCost.text = upgradeCost[abilityIndex].ToString();
+                abilityText.text = upgradeText[abilityIndex];
+                firstBar.sprite = firstFilled;
+                secondBar.sprite = secondFilled;
+                thirdBar.sprite = thirdEmpty;
+                break;
+            case 3:
+                coinCost.text = upgradeCost[abilityIndex].ToString();
+                abilityText.text = upgradeText[abilityIndex];
+                firstBar.sprite = firstFilled;
+                secondBar.sprite = secondFilled;
+                thirdBar.sprite = thirdFilled;
+                coinImage.gameObject.SetActive(false);
+                coinCost.gameObject.SetActive(false);
+                upgradeButton.SetActive(false);
+                maxedPanel.SetActive(true);
+                break;
+        }
+    }
+
+    public void ClosePanel(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+}
---
+++ GDDS 2/Assets/Jayden's Scripts/UpgradesAgain.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradesAgain : MonoBehaviour
+{
+    public Text coinText;
+    float coinCollected;
+
+    [Header("SwitchCharacter")]
+    public Image BG;
+    public GameObject Character;
+    public static string selectedCharacter;
+    public List<string> allChar;
+    public List<Sprite> charSprites;
+    public List<Sprite> bgSprites;
+    public List<RuntimeAnimatorController> charAnim;
+    int index;
+
+    [Header("Upgrades")]
+    public GameObject passivePanel;
+    public GameObject skillPanel;
+    public GameObject powerupPanel;
+    public GameObject healthPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinCollected = PlayerPrefs.GetFloat("Coins");
+        coinText.text = "=" + " " + coinCollected;
+        index = 0;
+        selectedCharacter = allChar[index];
+    }
+
+    public void SwitchCharacterLeft()
+    {
+        if(index -1 >= 0)
+        {
+            index--;
+            BG.sprite = bgSprites[index];
+            Character.GetComponent<Image>().sprite = charSprites[index];
+            Character.GetComponent<Animator>().runtimeAnimatorController = charAnim[index];
+            selectedCharacter = allChar[index];
+        }
+    }
+
+    public void SwitchCharacterRight()
+    {
+        if (index + 1 <= allChar.Count-1)
+        {
+            index++;
+            BG.sprite = bgSprites[index];
+            Character.GetComponent<Image>().sprite = charSprites[index];
+            Character.GetComponent<Animator>().runtimeAnimatorController = charAnim[index];
+            selectedCharacter = allChar[index];
+        }
+    }
+    
+    public void OpenPassive()
+    {
+        if (passivePanel.activeInHierarchy) return;
+        passivePanel.SetActive(true);
+    }
+
+    public void OpenSkill()
+    {
+        if (skillPanel.activeInHierarchy) return;
+        skillPanel.SetActive(true);
+    }
+
+    public void OpenPowerup()
+    {
+        if (powerupPanel.activeInHierarchy) return;
+        powerupPanel.SetActive(true);
+    }
+
+    public void OpenHealth()
+    {
+        if (healthPanel.activeInHierarchy) return;
+        healthPanel.SetActive(true);
+    }
+
+    public void UpgradePassive(PassivePanel ability)
+    {
+        if (coinCollected < ability.upgradeCost[ability.abilityIndex]) return;
+        PlayerPrefs.SetInt(selectedCharacter + "Passive", PlayerPrefs.GetInt(selectedCharacter + "Passive") + 1);
+        PlayerPrefs.SetFloat("Coins", coinCollected - ability.upgradeCost[ability.abilityIndex]);
+        coinCollected = PlayerPrefs.GetFloat("Coins");
+        coinText.text = "=" + " " + coinCollected;
+    }
+
+    public void UpgradeSkill(PassivePanel ability)
+    {
+        if (coinCollected < ability.upgradeCost[ability.abilityIndex]) return;
+        PlayerPrefs.SetInt(selectedCharacter + "Skill", PlayerPrefs.GetInt(selectedCharacter + "Skill") + 1);
+        PlayerPrefs.SetFloat("Coins", coinCollected - ability.upgradeCost[ability.abilityIndex]);
+        coinCollected = PlayerPrefs.GetFloat("Coins");
+        coinText.text = "=" + " " + coinCollected;
+    }
+
+    public void UpgradePoweup(PassivePanel ability)
+    {
+        if (coinCollected < ability.upgradeCost[ability.abilityIndex]) return;
+        PlayerPrefs.SetInt(selectedCharacter + "Powerup", PlayerPrefs.GetInt(selectedCharacter + "Powerup") + 1);
+        PlayerPrefs.SetFloat("Coins", coinCollected - ability.upgradeCost[ability.abilityIndex]);
+        coinCollected = PlayerPrefs.GetFloat("Coins");
+        coinText.text = "=" + " " + coinCollected;
+    }
+
+    public void UpgradeHealth(PassivePanel ability)
+    {
+        if (coinCollected < ability.upgradeCost[ability.abilityIndex]) return;
+        PlayerPrefs.SetInt(selectedCharacter + "Health", PlayerPrefs.GetInt(selectedCharacter + "Health") + 1);
+        PlayerPrefs.SetFloat("Coins", coinCollected - ability.upgradeCost[ability.abilityIndex]);
+        coinCollected = PlayerPrefs.GetFloat("Coins");
+        coinText.text = "=" + " " + coinCollected;
+    }
+}
